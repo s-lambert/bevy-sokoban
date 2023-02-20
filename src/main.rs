254f3e0b3907a0b0db6a0275f1028d274c225270
Scott Lambert <s-lambert@live.com.au>
2023-02-20 18:52:41 +0800

    Window setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: "Sokoban!".to_string(),
+                        width: 500.0,
+                        height: 500.0,
+                        ..default()
+                    },
+                    ..default()
+                }),
+        )
+        .add_system(bevy::window::close_on_esc)
+        .run();
 }
